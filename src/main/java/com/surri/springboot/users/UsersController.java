8d46c12148736b5b2b43f8b19597dd6d289d590d
surri <thekeh@hotmail.com>
2022-04-20 18:47:25 +0900

    hateoas

--- src/main/java/com/surri/springboot/users/UsersController.java
+++ src/main/java/com/surri/springboot/users/UsersController.java
@@ -12,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;;
 
 @RestController
 @RequestMapping("users")
@@ -34,14 +37,17 @@ public class UsersController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getById(@PathVariable Long id) {
-       Optional<User> user = usersService.findById(id);
+    public ResponseEntity<EntityModel<User>> findById(@PathVariable Long id) {
+        Optional<User> user = usersService.findById(id);
 
         if( !user.isPresent() ) {
             return ResponseEntity.notFound().build();
         }
 
-        return ResponseEntity.ok(user.get());
+        EntityModel<User> entityModel = EntityModel.of(user.get(),
+            linkTo(methodOn(UsersController.class).findById(id)).withSelfRel(),
+            linkTo(methodOn(UsersController.class).findAll()).withRel("users"));
+        return ResponseEntity.ok(entityModel);
     }
 
     @GetMapping("/ping")
